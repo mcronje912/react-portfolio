816aa55202690247827aa7a614140b146f4c281c
Marco Cronje <marcocronje@gmail.com>
2024-11-12 18:25:17 +0200

    fixing asset paths for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
 import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
@@ -10,7 +9,6 @@ function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
       <Route element={<PricingPage />} path="/pricing" />
       <Route element={<BlogPage />} path="/blog" />
       <Route element={<AboutPage />} path="/about" />
