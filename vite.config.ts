c697528496423e935f85a9f601b53063208d0660
Marco Cronje <marcocronje@gmail.com>
2024-11-12 16:21:46 +0200

    new test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/react-portfolio/",
+  base: "/react-portfolio/", // GitHub Pages requires this to match your repo name
   plugins: [react(), tsconfigPaths()],
 });
